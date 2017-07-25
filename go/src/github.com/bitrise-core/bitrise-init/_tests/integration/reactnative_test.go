ca64b2f21b019dd707a8a97d2ce67d52675e1f0d
Krisztian Godrei <godreikrisztian@gmail.com>
2017-07-25 16:53:27 +0200

    scanner update

--- go/src/github.com/bitrise-core/bitrise-init/_tests/integration/reactnative_test.go
+++ go/src/github.com/bitrise-core/bitrise-init/_tests/integration/reactnative_test.go
@@ -35,7 +35,126 @@ func TestReactNative(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, strings.TrimSpace(sampleAppsReactNativeIosAndAndroidResultYML), strings.TrimSpace(result))
 	}
+
+	t.Log("sample-apps-react-native-subdir")
+	{
+		sampleAppDir := filepath.Join(tmpDir, "sample-apps-react-native-subdir")
+		sampleAppURL := "https://github.com/bitrise-samples/sample-apps-react-native-subdir.git"
+		require.NoError(t, git.Clone(sampleAppURL, sampleAppDir))
+
+		cmd := command.New(binPath(), "--ci", "config", "--dir", sampleAppDir, "--output-dir", sampleAppDir)
+		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		require.NoError(t, err, out)
+
+		scanResultPth := filepath.Join(sampleAppDir, "result.yml")
+
+		result, err := fileutil.ReadStringFromFile(scanResultPth)
+		require.NoError(t, err)
+		require.Equal(t, strings.TrimSpace(sampleAppsReactNativeSubdirResultYML), strings.TrimSpace(result))
+	}
 }
+
+var sampleAppsReactNativeSubdirVersions = []interface{}{
+	models.FormatVersion,
+
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.ScriptVersion,
+	steps.InstallReactNativeVersion,
+	steps.NpmVersion,
+	steps.InstallMissingAndroidToolsVersion,
+	steps.GradleRunnerVersion,
+	steps.CertificateAndProfileInstallerVersion,
+	steps.XcodeArchiveVersion,
+	steps.DeployToBitriseIoVersion,
+
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.ScriptVersion,
+	steps.InstallReactNativeVersion,
+	steps.NpmVersion,
+	steps.NpmVersion,
+	steps.DeployToBitriseIoVersion,
+}
+
+var sampleAppsReactNativeSubdirResultYML = fmt.Sprintf(`options:
+  react-native:
+    title: Path to the gradle file to use
+    env_key: GRADLE_BUILD_FILE_PATH
+    value_map:
+      project/android/build.gradle:
+        title: Gradlew file path
+        env_key: GRADLEW_PATH
+        value_map:
+          project/android/gradlew:
+            title: Project (or Workspace) path
+            env_key: BITRISE_PROJECT_PATH
+            value_map:
+              project/ios/SampleAppsReactNativeAndroid.xcodeproj:
+                title: Scheme name
+                env_key: BITRISE_SCHEME
+                value_map:
+                  SampleAppsReactNativeAndroid:
+                    config: reactnative-android-ios-test-config
+                  SampleAppsReactNativeAndroid-tvOS:
+                    config: reactnative-android-ios-test-config
+configs:
+  react-native:
+    reactnative-android-ios-test-config: |
+      format_version: "%s"
+      default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+      project_type: react-native
+      trigger_map:
+      - push_branch: '*'
+        workflow: primary
+      - pull_request_source_branch: '*'
+        workflow: primary
+      workflows:
+        deploy:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - script@%s:
+              title: Do anything with Script step
+          - install-react-native@%s: {}
+          - npm@%s:
+              inputs:
+              - workdir: project
+              - command: install
+          - install-missing-android-tools@%s: {}
+          - gradle-runner@%s:
+              inputs:
+              - gradle_file: $GRADLE_BUILD_FILE_PATH
+              - gradle_task: assembleRelease
+              - gradlew_path: $GRADLEW_PATH
+          - certificate-and-profile-installer@%s: {}
+          - xcode-archive@%s:
+              inputs:
+              - project_path: $BITRISE_PROJECT_PATH
+              - scheme: $BITRISE_SCHEME
+              - configuration: Release
+          - deploy-to-bitrise-io@%s: {}
+        primary:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - script@%s:
+              title: Do anything with Script step
+          - install-react-native@%s: {}
+          - npm@%s:
+              inputs:
+              - workdir: project
+              - command: install
+          - npm@%s:
+              inputs:
+              - workdir: project
+              - command: test
+          - deploy-to-bitrise-io@%s: {}
+warnings:
+  react-native: []
+`, sampleAppsReactNativeSubdirVersions...)
 
 var sampleAppsReactNativeIosAndAndroidVersions = []interface{}{
 	models.FormatVersion,
