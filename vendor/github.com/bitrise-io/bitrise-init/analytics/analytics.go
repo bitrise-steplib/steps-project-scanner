0e25d79916bdce4481a09aa0d952d4975cf3ac6b
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-09-15 16:25:50 +0200

    Update bitrise-init dependency.

--- vendor/github.com/bitrise-io/bitrise-init/analytics/analytics.go
+++ vendor/github.com/bitrise-io/bitrise-init/analytics/analytics.go
@@ -1,17 +1,10 @@
 package analytics
 
 import (
-	"os"
-
 	"github.com/bitrise-io/go-utils/log"
 )
 
-// LogError sends analytics log using log.RErrorf by setting the stepID and data/build_slug.
+// LogError sends analytics log using log.RErrorf by setting the stepID.
 func LogError(tag string, data map[string]interface{}, format string, v ...interface{}) {
-	if data == nil {
-		data = map[string]interface{}{}
-	}
-	data["build_slug"] = os.Getenv("BITRISE_BUILD_SLUG")
-
 	log.RErrorf("bitrise-init", tag, data, format, v...)
 }
