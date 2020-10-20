2c13476f69715670ac2ef387c35a021d98d49691
doomsayer13 <doomsayer13@gmail.com>
2020-10-20 13:33:09 +0200

    dep update

--- vendor/github.com/bitrise-io/bitrise-init/models/models.go
+++ vendor/github.com/bitrise-io/bitrise-init/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/bitrise-io/bitrise-init/step"
+
 // BitriseConfigMap ...
 type BitriseConfigMap map[string]string
 
@@ -19,13 +21,23 @@ type Icon struct {
 // Icons is an array of icons
 type Icons []Icon
 
+// ErrorWithRecommendations ...
+type ErrorWithRecommendations struct {
+	Error           string
+	Recommendations step.Recommendation
+}
+
+// ErrorsWithRecommendations is an array with an Error and its Recommendations
+type ErrorsWithRecommendations []ErrorWithRecommendations
+
 // ScanResultModel ...
 type ScanResultModel struct {
-	ScannerToOptionRoot       map[string]OptionNode       `json:"options,omitempty" yaml:"options,omitempty"`
-	ScannerToBitriseConfigMap map[string]BitriseConfigMap `json:"configs,omitempty" yaml:"configs,omitempty"`
-	ScannerToWarnings         map[string]Warnings         `json:"warnings,omitempty" yaml:"warnings,omitempty"`
-	ScannerToErrors           map[string]Errors           `json:"errors,omitempty" yaml:"errors,omitempty"`
-	Icons                     []Icon                      `json:"-" yaml:"-"`
+	ScannerToOptionRoot               map[string]OptionNode                `json:"options,omitempty" yaml:"options,omitempty"`
+	ScannerToBitriseConfigMap         map[string]BitriseConfigMap          `json:"configs,omitempty" yaml:"configs,omitempty"`
+	ScannerToWarnings                 map[string]Warnings                  `json:"warnings,omitempty" yaml:"warnings,omitempty"`
+	ScannerToErrors                   map[string]Errors                    `json:"errors,omitempty" yaml:"errors,omitempty"`
+	ScannerToErrorsWithRecomendations map[string]ErrorsWithRecommendations `json:"errors_with_recommendations,omitempty" yaml:"errors_with_recommendations,omitempty"`
+	Icons                             []Icon                               `json:"-" yaml:"-"`
 }
 
 // AddError ...
