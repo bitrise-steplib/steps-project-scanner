4963ac08a0570c7bec585d54693dbf92074b2adb
Mate Herber <mate.herber@bitrise.io>
2020-11-09 17:56:27 +0100

    Fixed typo

--- submitresult.go
+++ submitresult.go
@@ -38,7 +38,7 @@ func newResultClient(resultSubmitURL string, resultSubmitAPIToken stepconf.Secre
 func (c *resultClient) uploadErrorResult(stepID string, err error) error {
 	if stepError, ok := err.(*step.Error); ok && len(stepError.Recommendations) > 0 {
 		return c.uploadResults(models.ScanResultModel{
-			ScannerToErrorsWithRecomendations: map[string]models.ErrorsWithRecommendations{
+			ScannerToErrorsWithRecommendations: map[string]models.ErrorsWithRecommendations{
 				"general": {
 					models.ErrorWithRecommendations{
 						Error:           fmt.Sprintf("Error in step %s: %v", stepID, stepError.Err),
