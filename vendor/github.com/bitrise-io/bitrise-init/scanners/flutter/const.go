624bb6f11755ef69336187fc212a369219cf6bd9
Szabolcs Toth <54896607+tothszabi@users.noreply.github.com>
2026-02-19 15:42:02 +0100

    Update init package (#168)

--- vendor/github.com/bitrise-io/bitrise-init/scanners/flutter/const.go
+++ vendor/github.com/bitrise-io/bitrise-init/scanners/flutter/const.go
@@ -4,15 +4,15 @@ const (
 	primaryWorkflowDescription = `Builds project and runs tests.
 
 Next steps:
-- Check out [Getting started with Flutter apps](https://devcenter.bitrise.io/en/getting-started/getting-started-with-flutter-apps.html).
+- Check out [Getting started with Flutter apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-flutter-projects.html).
 `
 
-	deployWorkflowDescription = `Builds and deploys app using [Deploy to bitrise.io Step](https://devcenter.bitrise.io/en/getting-started/getting-started-with-flutter-apps.html#deploying-a-flutter-app).
+	deployWorkflowDescription = `Builds and deploys app using [Deploy to bitrise.io Step](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-flutter-projects.html#deploying-a-flutter-app).
 
 If you build for iOS, make sure to set up code signing secrets on Bitrise for a successful build.
 
 Next steps:
-- Check out [Getting started with Flutter apps](https://devcenter.bitrise.io/en/getting-started/getting-started-with-flutter-apps.html) for signing and deployment options.
-- Check out the [Code signing guide](https://devcenter.bitrise.io/en/code-signing.html) for iOS and Android
+- Check out [Getting started with Flutter apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-flutter-projects.html) for signing and deployment options.
+- Check out the Code signing guide for [iOS](https://docs.bitrise.io/en/bitrise-ci/code-signing/ios-code-signing.html) and [Android](https://docs.bitrise.io/en/bitrise-ci/code-signing/android-code-signing.html).
 `
 )
--- vendor/github.com/bitrise-io/bitrise-init/scanners/reactnative/const.go
+++ vendor/github.com/bitrise-io/bitrise-init/scanners/reactnative/const.go
@@ -4,21 +4,21 @@ const (
 	deployWorkflowDescription = `Tests, builds and deploys the app using *Deploy to bitrise.io* Step.
 
 Next steps:
-- Set up an [Apple service with API key](https://devcenter.bitrise.io/en/accounts/connecting-to-services/connecting-to-an-apple-service-with-api-key.html).
-- Check out [Getting started with React Native apps](https://devcenter.bitrise.io/en/getting-started/getting-started-with-react-native-apps.html).
+- Set up an [Apple service with API key](https://docs.bitrise.io/en/bitrise-platform/integrations/apple-services-connection/connecting-to-an-apple-service-with-api-key.html).
+- Check out [Getting started with React Native apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-react-native-projects.html).
 `
 
 	primaryWorkflowDescription = `Runs tests.
 
 Next steps:
-- Check out [Getting started with React Native apps](https://devcenter.bitrise.io/en/getting-started/getting-started-with-react-native-apps.html).
+- Check out [Getting started with React Native apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-react-native-projects.html).
 `
 
 	primaryWorkflowNoTestsDescription = `Installs dependencies.
 
 Next steps:
 - Add tests to your project and configure the workflow to run them.
-- Check out [Getting started with React Native apps](https://devcenter.bitrise.io/en/getting-started/getting-started-with-react-native-apps.html).
+- Check out [Getting started with React Native apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-react-native-projects.html).
 `
 )
 
@@ -27,7 +27,7 @@ const (
 
 Next steps:
 - Configure the ` + "`Run Expo Application Services (EAS) build`" + ` Step's ` + "`Access Token`" + ` input.
-- Check out [Getting started with Expo apps](https://devcenter.bitrise.io/en/getting-started/getting-started-with-expo-apps.html).
+- Check out [Getting started with Expo apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-expo-projects.html).
 - For an alternative deploy workflow checkout the [(React Native) Expo: Build using Turtle CLI recipe](https://github.com/bitrise-io/workflow-recipes/blob/main/recipes/rn-expo-turtle-build.md).
 `
 
@@ -35,20 +35,20 @@ Next steps:
 
 Next steps:
 - Configure the ` + "`Run Expo Application Services (EAS) build`" + ` Step's ` + "`Access Token`" + ` input.
-- Check out [Getting started with Expo apps](https://devcenter.bitrise.io/en/getting-started/getting-started-with-expo-apps.html).
+- Check out [Getting started with Expo apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-expo-projects.html).
 - For an alternative deploy workflow checkout the [(React Native) Expo: Build using Turtle CLI recipe](https://github.com/bitrise-io/workflow-recipes/blob/main/recipes/rn-expo-turtle-build.md).
 `
 
 	expoPrimaryWorkflowDescription = `Runs tests.
 
 Next steps:
-- Check out [Getting started with Expo apps](https://devcenter.bitrise.io/en/getting-started/getting-started-with-expo-apps.html).
+- Check out [Getting started with Expo apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-expo-projects.html).
 `
 
 	expoPrimaryWorkflowNoTestsDescription = `Installs dependencies.
 
 Next steps:
 - Add tests to your project and configure the workflow to run them.
-- Check out [Getting started with Expo apps](https://devcenter.bitrise.io/en/getting-started/getting-started-with-expo-apps.html).
+- Check out [Getting started with Expo apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-expo-projects.html).
 `
 )
